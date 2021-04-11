dcfa5b9f8cfd0c0d4970b749e23790179846c530
Skyler Clark <wgahnagl@protonmail.com>
2021-04-11 15:52:22 -0400

    lxgui v2 update

--- src/main.cpp
+++ src/main.cpp
@@ -1,235 +1,168 @@
-#include <lxgui/gui_manager.hpp>
-#include <lxgui/gui_texture.hpp>
-#include <lxgui/gui_fontstring.hpp>
-#include <lxgui/gui_button.hpp>
-#include <lxgui/gui_slider.hpp>
-#include <lxgui/gui_editbox.hpp>
-#include <lxgui/gui_scrollframe.hpp>
-#include <lxgui/gui_statusbar.hpp>
+#include <lxgui/lxgui.hpp>
 #include <lxgui/gui_event.hpp>
 #include <lxgui/gui_out.hpp>
-#include <lxgui/luapp_state.hpp>
-#include <lxgui/luapp_function.hpp>
-#include <lxgui/utils_filesystem.hpp>
-#include <lxgui/utils_string.hpp>
+#include <lxgui/input.hpp>
 
-#include <sol/state.hpp>
-
-#include <SFML/Window.hpp>
-
-//#define GL_GUI
-
-#ifdef GL_GUI
-    #include <lxgui/impl/gui_gl_renderer.hpp>
-    #ifdef MACOSX
-        #include <OpenGL/gl.h>
-    #else
-        #include <GL/gl.h>
-    #endif
-#else
-    #include <lxgui/impl/gui_sfml.hpp>
-    #include <SFML/Graphics/RenderWindow.hpp>
-#endif
-
-#include <lxgui/impl/input_sfml_source.hpp>
-
-#ifdef WIN32
+#if defined(WIN32)
     #include <windows.h>
-    #ifdef MSVC
+    #if defined(MSVC)
         #pragma comment(linker, "/entry:mainCRTStartup")
     #endif
 #endif
 
-#include <fstream>
-#include <plog/Log.h>
-#include "plog/Initializers/RollingFileInitializer.h"
+#include <thread>
+#include <iostream>
 
-#define PROJECTNAME "unnamed-mmo"
+#include "common.hpp"
+
+#include <lxgui/impl/gui_gl_renderer.hpp>
+#include <lxgui/impl/input_sdl_source.hpp>
+#define SDL_MAIN_HANDLED
+#include <SDL2>
+#if defined(MACOSX)
+    #include <OpenGL/gl.h>
+#else
+    #include <GL/gl.h>
+#endif
 
 using namespace lxgui;
 
-// default window size
-int uiWindowWidth = 800;
-int uiWindowHeight = 600;
-bool bFullScreen = false; 
-bool bWindowedMode = false;
-std::string sLocale = "enGB";
-bool enableCaching = true;
-
-const char* BoolToString(bool b)
+// Helper class to manage the OpenGL context from SDL
+struct GLContext
 {
-  return b ? "true" : "false";
-}
+    SDL_GLContext pContext = nullptr;
 
-int main(int argc, char* argv[]){
-    // initialize logging 
-    plog::init(plog::debug, "logs/cout.txt");
+    explicit GLContext(SDL_Window* pWindow) : pContext(SDL_GL_CreateContext(pWindow))
+    {
+        if (pContext == nullptr)
+            throw gui::exception("SDL_GL_CreateContext", "Coult not create OpenGL context.");
+    }
 
-       try{
-        // Read the configuration data
-        if (utils::file_exists("config.lua")){ 
-            LOGD << "Reading from config.lua";
-            lua::state mLua; 
-            try{
-                mLua.do_file("config.lua");
-                uiWindowWidth  = mLua.get_global_int("window_width",  false, uiWindowWidth);
-                uiWindowHeight = mLua.get_global_int("window_height", false, uiWindowHeight); 
-                bFullScreen    = mLua.get_global_bool("fullscreen",   false, bFullScreen);
-                bWindowedMode  = mLua.get_global_bool("windowed_mode", false, bWindowedMode);
-                sLocale        = mLua.get_global_string("locale",     false, sLocale);
-                enableCaching  = mLua.get_global_bool("enable_caching", false, enableCaching);
-            }catch (...){
-                LOGD << "Error reading config file. using defaults";
-            }
-        }else{
-            // write config.lua file with defaults
-            LOGD << "Config file missing, creating config from defaults";
-            std::ofstream outfile("config.lua");
-            outfile << "locale = " + sLocale + ";" << std::endl; 
-            outfile << "window_width = " + std::to_string(uiWindowWidth) +";"<< std::endl;
-            outfile << "window_height = " + std::to_string(uiWindowHeight) + ";" << std::endl;
-            outfile << "fullscreen = " + std::string(BoolToString(bFullScreen)) + ";" << std::endl; 
-            outfile << "windowed_mode = " + std::string(BoolToString(bWindowedMode)) + ";" << std::endl;
-            outfile << "enable_caching = " + std::string(BoolToString(enableCaching)) + ";" << std::endl;
-            outfile.close();
-        } 
+    ~GLContext() noexcept { SDL_GL_DeleteContext(pContext); }
+};
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        // Redirect output from the gui library to the standard output.
+        // You can redirect it to a file, or your own logger, etc.
+        gui::out.rdbuf(std::cout.rdbuf());
 
         // Create a window
-        LOGD << "Creating window....";
-    #ifdef GL_GUI
-        sf::Window mWindow;
-    #else
-        sf::RenderWindow mWindow;
-    #endif
-        // set ui window size to max if windowed or fullscreen is enabled
-        if (bWindowedMode or bFullScreen){
-            uiWindowWidth = sf::VideoMode::getDesktopMode().width;
-            uiWindowHeight = sf::VideoMode::getDesktopMode().height;
+        std::cout << "Creating window..." << std::endl;
+        const std::string sWindowTitle = "test";
+        const uint uiWindowWidth  = 800;
+        const uint uiWindowHeight = 600;
+
+        if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        {
+            throw gui::exception("SDL_Init", "Could not initialise SDL: "+
+                std::string(SDL_GetError())+".");
         }
-        if (bFullScreen){
-            mWindow.create(sf::VideoMode(uiWindowWidth, uiWindowHeight, 32), PROJECTNAME, sf::Style::Fullscreen);
-        }
-        else{
-            mWindow.create(sf::VideoMode(uiWindowWidth, uiWindowHeight, 32), PROJECTNAME);
-        }
+
+        const std::uint32_t uiFlags =
+            SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI;
+
+        std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)> pWindow(
+            SDL_CreateWindow(
+                sWindowTitle.c_str(),
+                SDL_WINDOWPOS_UNDEFINED,
+                SDL_WINDOWPOS_UNDEFINED,
+                uiWindowWidth,
+                uiWindowHeight,
+                uiFlags
+            ),
+            &SDL_DestroyWindow
+        );
+
+        if (!pWindow)
+            throw gui::exception("SDL_Window", "Could not create window.");
+
+        // Create OpenGL context
+        GLContext mGLContext(pWindow.get());
+        SDL_GL_SetSwapInterval(0);
 
         // Initialize the gui
-        LOGD << "Creating gui manager....";
-        std::unique_ptr<gui::manager> pManager;
-
-    #ifdef GL_GUI
-        LOGD << "Using GL for gui";
-        // Define the GUI renderer
-        std::unique_ptr<gui::renderer_impl> pRendererImpl =
-            std::unique_ptr<gui::renderer_impl>(new gui::gl::renderer());
+        std::cout << "Creating gui manager..." << std::endl;
+        const std::string sLocale = "enGB";
 
         // Define the input manager
-        std::unique_ptr<input::source_impl> pInputSource;
-        // Use SFML (only implementation available for now)
-        pInputSource = std::unique_ptr<input::source_impl>(new input::sfml::source(mWindow));
+        std::unique_ptr<input::source> pInputSource;
+        {
+            bool bInitialiseSDLImage = true;
+            SDL_Renderer* pRenderer = nullptr;
+            pInputSource = std::unique_ptr<input::source>(new input::sdl::source(
+                pWindow.get(), pRenderer, bInitialiseSDLImage));
+        }
 
-        pManager = std::unique_ptr<gui::manager>(new gui::manager(
+        // Define the GUI renderer
+        std::unique_ptr<gui::renderer> pRenderer =
+            std::unique_ptr<gui::renderer>(new gui::gl::renderer(
+                pInputSource->get_window_width(),
+                pInputSource->get_window_height()));
+
+        // Create the GUI manager
+        std::unique_ptr<gui::manager> pManager = std::unique_ptr<gui::manager>(new gui::manager(
             // Provide the input source
             std::move(pInputSource),
-            // The locale
-            sLocale,
-            // Dimensions of the render window
-            mWindow.getSize().x, mWindow.getSize().y,
             // Provide the GUI renderer implementation
-            std::move(pRendererImpl)
+            std::move(pRenderer),
+            // The locale
+            sLocale
         ));
-    #else
-        LOGD << "Using sfml for gui";
-        // Use full SFML implementation
-        pManager = gui::sfml::create_manager(mWindow, sLocale);
-    #endif
 
-        pManager->enable_caching(enableCaching);
+        pManager->enable_caching(false);
 
-        // Load files :
-        //  - first set the directory in which the interface is located
-        pManager->add_addon_directory("interface");
-        //  - create the lua::state
-        LOGD << "Creating lua state...";
-        pManager->create_lua([](gui::manager& mManager) {
-            // We use a lambda function because this code might be called
-            // again later on, for example when one reloads the GUI (the
-            // lua state is destroyed and created again).
-            //  - register the needed widgets
-            mManager.register_region_type<gui::texture>();
-            mManager.register_region_type<gui::font_string>();
-            mManager.register_frame_type<gui::button>();
-            mManager.register_frame_type<gui::slider>();
-            mManager.register_frame_type<gui::edit_box>();
-            mManager.register_frame_type<gui::scroll_frame>();
-            mManager.register_frame_type<gui::status_bar>();
-            //  - register additional lua functions
-            sol::state& mSol = mManager.get_lua();
-            mSol.set_function("get_folder_list", [](const std::string& sDir) {
-                return utils::get_directory_list(sDir);
-            });
-            mSol.set_function("get_file_list", [](const std::string& sDir) {
-                return utils::get_file_list(sDir);
-            });
-        });
+        // Setup the GUI (see examples_common.cpp)
+        examples_setup_gui(*pManager);
 
-        //  - and load all files
-        LOGD << "Reading gui files..."; 
-        try{
-        pManager->read_files();
-        } catch (...) {
-            LOGE << "file read failed";
-
-        } 
-        
         // Start the main loop
         bool bRunning = true;
         bool bFocus = true;
-        // if you're inputting in a textbox, pause hotkeys 
-        bool textInputFocus = false;
-
-        float fDelta = 0.1f;
-        sf::Clock mClock, mPerfClock;
-        uint uiFrameCount = 0;
-
+        double fDelta = 0.0;
+        timing_clock::time_point mPrevTime = timing_clock::now();
         input::manager* pInputMgr = pManager->get_input_manager();
 
-        LOGD << "Entering main loop";
+        std::cout << "Entering loop..." << std::endl;
+
         while (bRunning)
         {
-            // Get events from SFML
-            sf::Event mEvent;
-            while (mWindow.pollEvent(mEvent))
+            // Get events from SDL
+            SDL_Event mEvent;
+            while (SDL_PollEvent(&mEvent))
             {
-                if (mEvent.type      == sf::Event::Closed)
-                    bRunning = false;
-                else if (mEvent.type == sf::Event::LostFocus) 
-                    bFocus = false;
-                else if (mEvent.type == sf::Event::GainedFocus)
-                    bFocus = true;
-                else if (mEvent.type == sf::Event::KeyReleased && !textInputFocus){
-                    switch (mEvent.key.code)
+                if (mEvent.type == SDL_WINDOWEVENT)
+                {
+                    if (mEvent.window.event == SDL_WINDOWEVENT_CLOSE)
+                        bRunning = false;
+                    else if (mEvent.window.event == SDL_WINDOWEVENT_FOCUS_LOST)
+                        bFocus = false;
+                    else if (mEvent.window.event == SDL_WINDOWEVENT_FOCUS_GAINED)
+                        bFocus = true;
+                }
+                else if (mEvent.type == SDL_KEYUP)
+                {
+                    // This uses events straight from SDL, but the GUI may want to
+                    // capture some of them (for example: the user is typing in an edit_box).
+                    // Therefore, before we can react to these events, we must check that
+                    // the input isn't being "focussed":
+                    if (!pInputMgr->is_keyboard_focused())
                     {
-                        case sf::Keyboard::Key::Escape:
-                            bRunning = false;
-                            break;
-                        case sf::Keyboard::Key::P:
-                            gui::out << pManager->print_ui() << std::endl;
-                            break;
-                        case sf::Keyboard::Key::K:
-                            gui::out << "###" << std::endl;
-                            break;
-                        case sf::Keyboard::Key::C:
-                            pManager->enable_caching(!pManager->is_caching_enabled());
-                            break;
-                        case sf::Keyboard::Key::R:
-                            pManager->reload_ui();
-                            break;
-                        default:
-                            break;
+                        switch (mEvent.key.keysym.sym)
+                        {
+                            case SDLK_ESCAPE:
+                                // Escape pressed: stop the program
+                                bRunning = false;
+                                break;
+                            default:
+                                break;
+                        }
                     }
                 }
-                static_cast<input::sfml::source*>(pInputMgr->get_source())->on_sfml_event(mEvent);
+
+                // Feed events to the GUI
+                static_cast<input::sdl::source*>(pInputMgr->get_source())->on_sdl_event(mEvent);
             }
 
             // Check if WORLD input is allowed
@@ -238,48 +171,48 @@ int main(int argc, char* argv[]){
                 // Process mouse and click events in the game...
             }
 
+            // If the window is not focussed, do nothing and wait until focus comes back
             if (!bFocus)
             {
-                sf::sleep(sf::seconds(0.1f));
+                std::this_thread::sleep_for(std::chrono::milliseconds(100));
                 continue;
             }
 
             // Update the gui
+            SDL_GL_MakeCurrent(pWindow.get(), mGLContext.pContext);
             pManager->update(fDelta);
 
-            // Clear the window
-        #ifdef GL_GUI
+            // Your own rendering would go here!
+            // For this example, we just clear the window
             glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
-        #else
-            mWindow.clear(sf::Color(51,51,51));
-        #endif
 
-            // Render the gui
+            // Render the gui on top of the world
             pManager->render_ui();
 
             // Display the window
-            mWindow.display();
+            SDL_GL_SwapWindow(pWindow.get());
 
-            fDelta = mClock.getElapsedTime().asSeconds();
-            mClock.restart();
-
-            ++uiFrameCount;
+            // Compute time delta since last frame
+            timing_clock::time_point mCurrentTime = timing_clock::now();
+            fDelta = get_time_delta(mPrevTime, mCurrentTime);
+            mPrevTime = mCurrentTime;
         }
-        LOGD << "End of loop, mean FPS : " << uiFrameCount/mPerfClock.getElapsedTime().asSeconds();
+
+        std::cout << "End of loop." << std::endl;
     }
     catch (const std::exception& e)
     {
-        LOGE << e.what();
+        std::cout << e.what() << std::endl;
         return 1;
     }
     catch (...)
     {
-        LOGE << "Unhandled exception !";
+        std::cout << "# Error # : Unhandled exception !" << std::endl;
         return 1;
     }
 
-    LOGD << "End of program.";
+    std::cout << "End of program." << std::endl;
+
     return 0;
 }
-
---
+++ src/main2.cpp
@@ -0,0 +1,285 @@
+#include <lxgui/gui_manager.hpp>
+#include <lxgui/gui_texture.hpp>
+#include <lxgui/gui_fontstring.hpp>
+#include <lxgui/gui_button.hpp>
+#include <lxgui/gui_slider.hpp>
+#include <lxgui/gui_editbox.hpp>
+#include <lxgui/gui_scrollframe.hpp>
+#include <lxgui/gui_statusbar.hpp>
+#include <lxgui/gui_event.hpp>
+#include <lxgui/gui_out.hpp>
+#include <lxgui/luapp_state.hpp>
+#include <lxgui/luapp_function.hpp>
+#include <lxgui/utils_filesystem.hpp>
+#include <lxgui/utils_string.hpp>
+
+#include <sol/state.hpp>
+
+#include <SFML/Window.hpp>
+
+//#define GL_GUI
+
+#ifdef GL_GUI
+    #include <lxgui/impl/gui_gl_renderer.hpp>
+    #ifdef MACOSX
+        #include <OpenGL/gl.h>
+    #else
+        #include <GL/gl.h>
+    #endif
+#else
+    #include <lxgui/impl/gui_sfml.hpp>
+    #include <SFML/Graphics/RenderWindow.hpp>
+#endif
+
+#include <lxgui/impl/input_sfml_source.hpp>
+
+#ifdef WIN32
+    #include <windows.h>
+    #ifdef MSVC
+        #pragma comment(linker, "/entry:mainCRTStartup")
+    #endif
+#endif
+
+#include <fstream>
+#include <plog/Log.h>
+#include "plog/Initializers/RollingFileInitializer.h"
+
+#define PROJECTNAME "unnamed-mmo"
+
+using namespace lxgui;
+
+// default window size
+int uiWindowWidth = 800;
+int uiWindowHeight = 600;
+bool bFullScreen = false; 
+bool bWindowedMode = false;
+std::string sLocale = "enGB";
+bool enableCaching = true;
+
+const char* BoolToString(bool b)
+{
+  return b ? "true" : "false";
+}
+
+int main(int argc, char* argv[]){
+    // initialize logging 
+    plog::init(plog::debug, "logs/cout.txt");
+
+       try{
+        // Read the configuration data
+        if (utils::file_exists("config.lua")){ 
+            LOGD << "Reading from config.lua";
+            lua::state mLua; 
+            try{
+                mLua.do_file("config.lua");
+                uiWindowWidth  = mLua.get_global_int("window_width",  false, uiWindowWidth);
+                uiWindowHeight = mLua.get_global_int("window_height", false, uiWindowHeight); 
+                bFullScreen    = mLua.get_global_bool("fullscreen",   false, bFullScreen);
+                bWindowedMode  = mLua.get_global_bool("windowed_mode", false, bWindowedMode);
+                sLocale        = mLua.get_global_string("locale",     false, sLocale);
+                enableCaching  = mLua.get_global_bool("enable_caching", false, enableCaching);
+            }catch (...){
+                LOGD << "Error reading config file. using defaults";
+            }
+        }else{
+            // write config.lua file with defaults
+            LOGD << "Config file missing, creating config from defaults";
+            std::ofstream outfile("config.lua");
+            outfile << "locale = " + sLocale + ";" << std::endl; 
+            outfile << "window_width = " + std::to_string(uiWindowWidth) +";"<< std::endl;
+            outfile << "window_height = " + std::to_string(uiWindowHeight) + ";" << std::endl;
+            outfile << "fullscreen = " + std::string(BoolToString(bFullScreen)) + ";" << std::endl; 
+            outfile << "windowed_mode = " + std::string(BoolToString(bWindowedMode)) + ";" << std::endl;
+            outfile << "enable_caching = " + std::string(BoolToString(enableCaching)) + ";" << std::endl;
+            outfile.close();
+        } 
+
+        // Create a window
+        LOGD << "Creating window....";
+    #ifdef GL_GUI
+        sf::Window mWindow;
+    #else
+        sf::RenderWindow mWindow;
+    #endif
+        // set ui window size to max if windowed or fullscreen is enabled
+        if (bWindowedMode or bFullScreen){
+            uiWindowWidth = sf::VideoMode::getDesktopMode().width;
+            uiWindowHeight = sf::VideoMode::getDesktopMode().height;
+        }
+        if (bFullScreen){
+            mWindow.create(sf::VideoMode(uiWindowWidth, uiWindowHeight, 32), PROJECTNAME, sf::Style::Fullscreen);
+        }
+        else{
+            mWindow.create(sf::VideoMode(uiWindowWidth, uiWindowHeight, 32), PROJECTNAME);
+        }
+
+        // Initialize the gui
+        LOGD << "Creating gui manager....";
+        std::unique_ptr<gui::manager> pManager;
+
+    #ifdef GL_GUI
+        LOGD << "Using GL for gui";
+        // Define the GUI renderer
+        std::unique_ptr<gui::renderer_impl> pRendererImpl =
+            std::unique_ptr<gui::renderer_impl>(new gui::gl::renderer());
+
+        // Define the input manager
+        std::unique_ptr<input::source_impl> pInputSource;
+        // Use SFML (only implementation available for now)
+        pInputSource = std::unique_ptr<input::source_impl>(new input::sfml::source(mWindow));
+
+        pManager = std::unique_ptr<gui::manager>(new gui::manager(
+            // Provide the input source
+            std::move(pInputSource),
+            // The locale
+            sLocale,
+            // Dimensions of the render window
+            mWindow.getSize().x, mWindow.getSize().y,
+            // Provide the GUI renderer implementation
+            std::move(pRendererImpl)
+        ));
+    #else
+        LOGD << "Using sfml for gui";
+        // Use full SFML implementation
+        pManager = gui::sfml::create_manager(mWindow, sLocale);
+    #endif
+
+        pManager->enable_caching(enableCaching);
+
+        // Load files :
+        //  - first set the directory in which the interface is located
+        pManager->add_addon_directory("interface");
+        //  - create the lua::state
+        LOGD << "Creating lua state...";
+        pManager->create_lua([](gui::manager& mManager) {
+            // We use a lambda function because this code might be called
+            // again later on, for example when one reloads the GUI (the
+            // lua state is destroyed and created again).
+            //  - register the needed widgets
+            mManager.register_region_type<gui::texture>();
+            mManager.register_region_type<gui::font_string>();
+            mManager.register_frame_type<gui::button>();
+            mManager.register_frame_type<gui::slider>();
+            mManager.register_frame_type<gui::edit_box>();
+            mManager.register_frame_type<gui::scroll_frame>();
+            mManager.register_frame_type<gui::status_bar>();
+            //  - register additional lua functions
+            sol::state& mSol = mManager.get_lua();
+            mSol.set_function("get_folder_list", [](const std::string& sDir) {
+                return utils::get_directory_list(sDir);
+            });
+            mSol.set_function("get_file_list", [](const std::string& sDir) {
+                return utils::get_file_list(sDir);
+            });
+        });
+
+        //  - and load all files
+        LOGD << "Reading gui files..."; 
+        try{
+        pManager->read_files();
+        } catch (...) {
+            LOGE << "file read failed";
+
+        } 
+        
+        // Start the main loop
+        bool bRunning = true;
+        bool bFocus = true;
+        // if you're inputting in a textbox, pause hotkeys 
+        bool textInputFocus = false;
+
+        float fDelta = 0.1f;
+        sf::Clock mClock, mPerfClock;
+        uint uiFrameCount = 0;
+
+        input::manager* pInputMgr = pManager->get_input_manager();
+
+        LOGD << "Entering main loop";
+        while (bRunning)
+        {
+            // Get events from SFML
+            sf::Event mEvent;
+            while (mWindow.pollEvent(mEvent))
+            {
+                if (mEvent.type      == sf::Event::Closed)
+                    bRunning = false;
+                else if (mEvent.type == sf::Event::LostFocus) 
+                    bFocus = false;
+                else if (mEvent.type == sf::Event::GainedFocus)
+                    bFocus = true;
+                else if (mEvent.type == sf::Event::KeyReleased && !textInputFocus){
+                    switch (mEvent.key.code)
+                    {
+                        case sf::Keyboard::Key::Escape:
+                            bRunning = false;
+                            break;
+                        case sf::Keyboard::Key::P:
+                            gui::out << pManager->print_ui() << std::endl;
+                            break;
+                        case sf::Keyboard::Key::K:
+                            gui::out << "###" << std::endl;
+                            break;
+                        case sf::Keyboard::Key::C:
+                            pManager->enable_caching(!pManager->is_caching_enabled());
+                            break;
+                        case sf::Keyboard::Key::R:
+                            pManager->reload_ui();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                static_cast<input::sfml::source*>(pInputMgr->get_source())->on_sfml_event(mEvent);
+            }
+
+            // Check if WORLD input is allowed
+            if (pInputMgr->can_receive_input("WORLD"))
+            {
+                // Process mouse and click events in the game...
+            }
+
+            if (!bFocus)
+            {
+                sf::sleep(sf::seconds(0.1f));
+                continue;
+            }
+
+            // Update the gui
+            pManager->update(fDelta);
+
+            // Clear the window
+        #ifdef GL_GUI
+            glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+        #else
+            mWindow.clear(sf::Color(51,51,51));
+        #endif
+
+            // Render the gui
+            pManager->render_ui();
+
+            // Display the window
+            mWindow.display();
+
+            fDelta = mClock.getElapsedTime().asSeconds();
+            mClock.restart();
+
+            ++uiFrameCount;
+        }
+        LOGD << "End of loop, mean FPS : " << uiFrameCount/mPerfClock.getElapsedTime().asSeconds();
+    }
+    catch (const std::exception& e)
+    {
+        LOGE << e.what();
+        return 1;
+    }
+    catch (...)
+    {
+        LOGE << "Unhandled exception !";
+        return 1;
+    }
+
+    LOGD << "End of program.";
+    return 0;
+}
+
